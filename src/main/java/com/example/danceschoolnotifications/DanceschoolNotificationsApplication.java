11a70558aceae00330e6f0b6e0178b41605f4ed8
daris <daris91@gmail.com>
2025-11-27 08:49:59 +0100

    initial commit

---
+++ src/main/java/com/example/danceschoolnotifications/DanceschoolNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.danceschoolnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceschoolNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceschoolNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/danceschoolnotifications/DanceschoolNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danceschoolnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceschoolNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
